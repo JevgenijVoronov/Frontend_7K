128f57cf2a78b81c92d24eb73076fa9a0a998d7a
JevgenijVoronov <jevgenijsvoronovs@gmail.com>
2022-05-10 07:10:18 +0300

    06-md-solution

--- Majasdarbs/06-majasdarbs/script.js
+++ Majasdarbs/06-majasdarbs/script.js
@@ -1,18 +1,28 @@
-let characters = ['a','b','c','d','e','f','g','h','i','j','k,','l','m','n','o','p','q','r','s','t','u','v','w','x,','y','z'];
-
-let arrayId = prompt("Ievadiet skaitli no 0 lidz 26", );
-
+const characters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let arrayId = ''
 let output = document.getElementById('main');
-output.innerText = characters[arrayId];
+
+let getValue = () => {
+    arrayId = prompt(`Ievadiet skaitli no 0 lidz 25`,);
+    if (arrayId > 25 || arrayId === '') {
+        alert(`Skaitlis ${arrayId} neatbilst intervālam no 0 lidz 25`);
+        getValue();
+    } else {
+        printValue();
+    }
+}
+
+let printValue = () => {
+    output.innerText = characters[arrayId];
+}
+
+getValue();
 
 document.addEventListener("keypress", (eventObject) => {
     if (eventObject.key === characters[arrayId]) {
-        console.log('yes');
+        alert('PAREIZI');
+        getValue();
     } else {
-        console.log('no');
+        alert('NEPAREIZI');
     }
 })
-
-
-
-
