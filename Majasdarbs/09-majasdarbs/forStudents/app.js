7d0bfe7bfa0db10ffbee6b4c1a2cb0d4bedc943e
Rudolfs Dudarevs <mans.epats@gmail.com>
2022-06-06 10:44:25 +0300

    09: homework spellcheck

--- Majasdarbs/09-majasdarbs/forStudents/app.js
+++ Majasdarbs/09-majasdarbs/forStudents/app.js
@@ -1,32 +1,32 @@
 /* 
-    Šaja majasdarba mes izveidosiem nelilu aplikaciju kura izvadis uz ekrana izveleto siņa šķirni bildi
+    Šajā majasdarba mes izveidosim nelielu aplikāciju, kura izvadīs uz ekrāna izvēlēto suņa šķirnes bildi.
     Apkskatiet DogApiDemo.mp4 
 
-    Mes izmantosim API https://dog.ceo/dog-api/
+    Izmantosim API https://dog.ceo/dog-api/
 
-    1)  Apskatiet failu data.js tur piejami visi dati par suņu šķirnem
-        Mums ir piejajams <select id='dogSelector'> kura mums nepiecišams pievienot vairakas <option value="value">name</option>
-        Mes to varam izveidot ar rokam bet labak izmantot ciklu kurš izies cauri visiem datim un izveidos mums nepiecišamus elementus
+    1)  Apskatiet failu data.js tur piejami visi dati par suņu šķirnēm.
+        Mums ir piejajams <select id='dogSelector'> kurā nepiecišams pievienot vairākus <option value="value">name</option>
+        Mēs varētu katru <option> elementu ierakstīt HTML, bet labak izmantot ciklu kurš izies cauri visiem datiem un izveidos mums nepiecišamos elementus
 
-        Implementacija: 
-        - Izvejdosim mainigo dogSelector kura saglabasim musu <select> element var izmantot elementa id 'dogSelector' un getElementById() metodi
-        - izveidosiem funkciju renderDogData(data) kura saņems siņa šķirnes datus 
-        - izmantojot cilku forEach() izesiem cauri musu datiem un pieinosim musu dogSelector ar innerHTML metodi nepiecišamo html
+        Implementācija: 
+        - Izvejdosim mainīgo dogSelector kurā saglabāsim mūsu <select> element var izmantot elementa id 'dogSelector' un getElementById() metodi
+        - izveidosim funkciju renderDogData(data) kura saņems suņa šķirnes datus 
+        - izmantojot cilku forEach() izesiem cauri datiem un pievienosim mūsu dogSelector elementam ar innerHTML nepiecišamo html:
             `<option value=${breed.value}>${breed.name}</option>`
-        - izsaucam musu funkciju un rezultata mes saņemis visas iespejamas suņu šķirnes
+        - izsaucam renderDogData() funkciju un rezultātā iegūsim visas iespejamāss suņu šķirnes iekš <select> elementa kā <options>.
 
 
 
 
-    2)  Izmantojot addEventListener('change', funkcijaVards) notikumu pirkš dogSelector mes varam palaist funkciju kura sanems mums siņa bildi
+    2)  Izmantojot addEventListener('change', getDogImg) notikumu priekš dogSelector elementa, piesaistam šim notikumam funkciju kura kā parametru saņemt suņa attēlu.
 
-        Implementacija:
-        - pievienojam addEventListener pie dogSelector un izsaucam funkciju getDogImg
+        Implementācija:
+        - pievienojam addEventListener pie dogSelector uz un piesaistām tam funkciju getDogImg
         - pievinojam funkciju getDogImg
-        - fukcija pievienojam mainigu url kura mes saglabasim musi api url
+        - fukcijā izveidojam string tipa mainīgo, kas saturēs API url priekš konkrētās suņa šķirnes. Šajā string vērtībā dinamiski jāievieto suņa šķirnes nosaukums no <select> elementa value attiecīgajā URL daļā. 
             https://dog.ceo/api/breed/SUŅA-ŠKIRNE/images/random
-        - musu api nepiecišams padot izveleto sunā škirni varam to padot devinejot dogSelector.value
-        talak uzmantojot fetch() metodi izvejdojam request prikš servera
+        - iegūstam nepieciešamo suņa šķirnes nosaukumu, izmantojot elementa dogSelector.value
+          tālak izmantojot fetch() metodi izvejdojam request priekš servera
             fetch(url)
                 .then(response => response.json())
                 .then(data => renderDogData(data))
@@ -36,9 +36,9 @@
 
     3)  Izvadit suņa bildi 
         
-        Implementacija:
-        - izvedojam funkciju renderDogData() kurai padosim musu data objektu 
-        - ivejdosim vel vienu selektoru lai dabut musu dogImg elementu 
+        Implementācija:
+        - izvedojam funkciju renderDogData() kurai kā parametru padosim mūsu data objektu 
+        - ivejdosim vēl vienu selektoru lai dabūt mūsu dogImg elementu 
         - izmantojot innerHTML pievienot img tag ar src=${data.message}
 */
 
